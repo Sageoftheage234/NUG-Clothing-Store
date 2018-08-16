4b8cf7ec48c58b696e25d54444d3feab44bb66d3
sageoftheage234 <jourdane234@yahoo.com>
2018-08-15 20:28:57 -0400

    stores created as object literals

---
+++ main.js
@@ -0,0 +1,40 @@
+// Clothing Properties
+
+//clothingTypeArray [Tops, Jeans, Trousers, Undergarments, Shirts, Dresses, Outerwear, Coats, Footwear, Headgear, Jackets]
+// clothingSizeArray [XS, S, M, L, XL, XXL]
+//clothingGender [Men's, Women's]
+//s
+
+
+let Store1 ={
+    name: 'Chocolate Droppa',
+    type: ['Mint', 'Peanut Butter', 'Macadamia Nut'],
+    location: 'Silver Spring',
+    minCust:5,
+    maxCust:100,
+    avgCookie:3,
+    openTime:0900,
+    closeTime:1800,
+}
+
+let Store2={
+    name: 'Raider\'s Rocky Raisin Rocket',
+    type: ['Oatmeal Raisin','Cinnamon Raisin', 'Granola', 'Choco-Granola' ],
+    location:'Rockville',
+    minCust:10,
+    maxCust: 150,
+    avgCookie: 10.3,
+    openTime: 0800,
+    closeTime: 2100,
+}
+
+let Store3={
+    name: 'DC Bakery',
+    type: ['Sugar', 'Chocolate Chip', 'Birthday Cake', 'Baker\'s Remix'],
+    location: 'Georgetown',
+    minCust:2,
+    maxCust: 200,
+    avgCookie:20,
+    openTime:0800,
+    closeTime:1500,
+}
