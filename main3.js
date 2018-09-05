9aeb2de3cf5f8c9369e799c55a817c2d4ae12f2a
sageoftheage234 <jourdane234@yahoo.com>
2018-09-05 03:26:11 -0400

    debugged the formula issue and presented both footers successfully

--- main3.js
+++ main3.js
@@ -4,13 +4,13 @@ let elStoreTable = document.getElementById('Store-Table');
 let elForm = document.getElementById('Store-form')
 
 let Store = function(nameofStore,locationOfStore, typeOfCookiesSoldAtStore, minimumNumberOfCustomers,
-    maximumNumberOfCustomers,averageCookiesSoldPerDay,openTimeForStore,closeTimeForStore){
+    maximumNumberOfCustomers,averageCookiesSoldPerCustomer,openTimeForStore,closeTimeForStore){
     this.name = nameofStore
     this.location = locationOfStore
     this.type = typeOfCookiesSoldAtStore
     this.minCust = minimumNumberOfCustomers
     this.maxCust = maximumNumberOfCustomers
-    this.avgCookie = averageCookiesSoldPerDay
+    this.avgCookie = averageCookiesSoldPerCustomer
     this.openTime = openTimeForStore
     this.closeTime = closeTimeForStore
     this.totalCookiesPerDay = 0
@@ -28,10 +28,13 @@ let Store4 = new Store ('Willy Cronka\'s','Gaithersburg', ['Booga Suga','Mama\'s
 
 storeArray.push(Store1,Store2,Store3,Store4);
 
-
+//declared the displayTableHeader function 
 function displayTableHeader(){
-    let elRow = document.createElement('tr');
+    //declare variable which assigns the element 'tr' as table row into HTML tag
+    let elRow = document.createElement('tr'); 
+    // elStoreTable variable accesses the appendchild method to pass the elRow argument
     elStoreTable.appendChild(elRow);
+    // declare the elNameHeader variable and assigned the element 'th' to pass the pa
     let elNameHeader = document.createElement('th');
     elRow.appendChild(elNameHeader);
     elNameHeader.innerHTML = 'Name of Store';
@@ -46,6 +49,8 @@ function displayTableHeader(){
     let elTotalHeader = document.createElement('th');
     elRow.appendChild(elTotalHeader);
     elTotalHeader.innerHTML ='Total';
+
+    
 };
 
 
@@ -62,7 +67,7 @@ let  displayTotalCookiesSoldPerHour= function(store){
         elRow.appendChild(elTableData);
         elTableData.innerHTML = result;
         store.totalCookiesPerDay += result;    
-       // elTableData.innerHTML = store.totalCookiesPerHour();
+        store.salesPerHourArray.push(result);
         };
     let elTotalStoreTableData = document.createElement('th');
     elRow.appendChild(elTotalStoreTableData);
@@ -70,25 +75,79 @@ let  displayTotalCookiesSoldPerHour= function(store){
     
 };
 
-function displayFooter(){
+let sTCPHArray = [];
 
+let total = 0;
+function calcFooter(){
+    for(let i = 0; i < 13; i++){
+        let storeTotalCookiesPerHour = 0;
+        for (let j = 0; j < storeArray.length;j++){
+                storeTotalCookiesPerHour += storeArray[j].salesPerHourArray[i];
+            };
+
+        total += storeTotalCookiesPerHour;
+        sTCPHArray.push(storeTotalCookiesPerHour);
+        console.log(total)
+    };
     
-}
+    let elTotalSalesPerHourFooter = document.createElement('th');
+    elTotalSalesPerHourFooter.setAttribute('class','footer');
 
+    let elallStoreTotalSalesPerHourFooter = document.createElement('th');
+    elallStoreTotalSalesPerHourFooter.setAttribute('class','footer');
 
-
-
+};
+ console.log('sum of hour column' , sTCPHArray);
 
 let storeName = elForm.storeName;
-//let CookiesPerFlavor = elForm. //
-
+let cookiesPerCustomer = elForm.cookiesPerCustomer
+let customerMin = elForm.customerMin
+let customerMax = elForm.customerMax
 
 
 function createNewStore(event){
     event.preventDefault();
-    let newStore = new Store(storeName.value, 10, 50,30,8,21)
+    let newStore = new Store(storeName.value, '','',parseInt(customerMin.value),parseInt(customerMax.value), parseInt(cookiesPerCustomer.value) ,8,21);
     console.log(newStore);
+    elStoreTable.removeChild(elStoreTable.childNodes[elStoreTable.childNodes.length - 1])
+    storeArray.push(newStore);
+    console.log('storeArray', storeArray);
+    
     displayTotalCookiesSoldPerHour(newStore);
+    let elRowFooter = document.createElement('tr');
+    let elFooterTitle = document.createElement('th');
+    elRowFooter.appendChild(elFooterTitle)
+    elFooterTitle.innerHTML = 'Total';
+     
+    let sTCPHSArray = [];
+    let total = 0;
+    for(let i = 0; i < 13; i++){
+        let storeTotalCookiesPerHour = 0;
+        for (let j = 0; j < storeArray.length;j++){
+            storeTotalCookiesPerHour += storeArray[j].salesPerHourArray[i];
+        };
+        total += storeTotalCookiesPerHour;
+        sTCPHSArray.push(storeTotalCookiesPerHour);
+        console.log('sum of updated hour column', sTCPHSArray)
+   
+    };
+    console.log(total)
+
+    elStoreTable.appendChild(elRowFooter);
+
+    for (let i = 0; i < 13; i++){
+        let elTotalSalesPerHourFooter = document.createElement('th');
+        elRowFooter.appendChild(elTotalSalesPerHourFooter);
+        elTotalSalesPerHourFooter.innerHTML = sTCPHSArray[i];
+    };
+    console.log('sTCPHSArray', sTCPHSArray);
+    
+    let elFinalFooterTitle = document.createElement('th')
+    elRowFooter.appendChild(elFinalFooterTitle);
+    elFinalFooterTitle.innerHTML = total;
+    console.log('elFinalFooterTitle', total)
+    
+
 };
 
 elForm.addEventListener('submit', createNewStore);
@@ -98,10 +157,34 @@ function populateTable(){
     displayTableHeader();
     for(let i = 0; i < storeArray.length;i++){
         displayTotalCookiesSoldPerHour(storeArray[i]);
-    
     };
+    calcFooter();
+
+};
+
+
+function populateFooter(){
+    let elRowFooter = document.createElement('tr');
+    elStoreTable.appendChild(elRowFooter);
+    let elFooterTitle = document.createElement('th');
+    elRowFooter.appendChild(elFooterTitle)
+    elFooterTitle.innerHTML = 'Total';
+   // let elRow = document.createElement('tr');
+   // elStoreTable.appendChild(elRow);
+   // let elRowFooter = document.createElement('tr')
+
+    for (let i = 0; i <sTCPHArray.length; i++){
+    let elTotalSalesPerHourFooter = document.createElement('th');
+    elRowFooter.appendChild(elTotalSalesPerHourFooter);
+    elTotalSalesPerHourFooter.innerHTML = sTCPHArray[i];
+    console.log('sTCPHArray', sTCPHArray);
+    };
+   // let elFinalFooter = document.createElement('tr');
+   // elStoreTable.appendChild(elFinalFooter);
+   let elFinalFooterTitle = document.createElement('th')
+   elRowFooter.appendChild(elFinalFooterTitle);
+   elFinalFooterTitle.innerHTML = total;
 };
 
 populateTable();
-
-
+populateFooter();
